0e1c0f537795df770f0bb6d786909188bdfb741c
Evelynall <Evelynalll@outlook.com>
2023-07-19 15:36:24 +0800

    Site updated: 2023-07-19 15:36:24

---
+++ interesting/js/search.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  // 原有JavaScript代码
+
+  const searchInput = document.getElementById('searchInput');
+
+
+  searchInput.addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+      let keyword = searchInput.value;
+      window.location.href = `https://cn.bing.com/search?pglt=163&q=${keyword}`;
+    }
+  });
+  
+}); 
